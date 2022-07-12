aa2d6c0c1592201fc6d0142c6d4bab89f7e79fea
nikhilsinghns07 <nikhilsinghns01@gmail.com>
2022-07-12 23:20:31 +0530

    Initialized Discussion Forum MERN App

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const bodyparser = require('body-parser')
+const PORT = process.env.PORT || 3001
+const mongoose = require('mongoose')
+const MONGO_URL = 'mongodb+srv://Nikhil0799:Nikhil0799@cluster0.5xymi.mongodb.net/Dforum'
+
+app.use(cors())
+app.use(bodyparser.json())
+
+mongoose.connect(MONGO_URL).then(res => {
+    console.log('Mongo Running')
+}).catch(error => {
+    console.log(error)
+})
+
+app.listen(PORT,() => {
+    console.log(`Server running on ${PORT}`)
+})
